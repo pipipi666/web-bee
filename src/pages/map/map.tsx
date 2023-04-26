9d6dfca148906a6ce3888f328ef02790d0063977
pipipi666 <nnm107072@gmail.com>
2023-04-26 06:22:30 +0300

    feat: add preloader

--- src/pages/map/map.tsx
+++ src/pages/map/map.tsx
@@ -1,5 +1,15 @@
+import styles from "./map.module.scss";
 import Container from "../../components/container/container";
+import PreloaderIcon from "../../components/UI/Icons/preloader";
 
 export default function MapPage() {
-  return <Container title="Basic map">map</Container>;
+  return (
+    <Container title="Basic map">
+      <div className={styles.map}>
+        <div className={styles.preloader}>
+          <PreloaderIcon />
+        </div>
+      </div>
+    </Container>
+  );
 }
