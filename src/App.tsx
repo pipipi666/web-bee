b75f2d56d09bd18ffd79f9f30ebb9f601e25b848
pipipi666 <nnm107072@gmail.com>
2023-07-02 14:59:33 +0300

    refactor: unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProfilePage from "pages/profile/profile";
 import MapPage from "pages/map-location/map-location";
